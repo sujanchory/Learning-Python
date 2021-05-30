71357ec25425dd39c3b622f35d1f270b7ecc9cdf
Sujan Peddinti <20mh1a4932@acoe.edu.in>
2021-05-30 08:49:28 +0530

    Add files via upload

---
+++ Elif(Credentials).py
@@ -0,0 +1,10 @@
+uid=(input())
+upw=(input())
+
+if uid=='thub@123':
+    if upw== 'python123':
+        print('Wrong Password')
+    else:
+            print('Wrong password')
+else:
+     print('Wrong ID')
