c20f9389bdbb675f8d8b691bac0166145e55e1d6
Sujan Peddinti <sujanchory@gmail.com>
2021-06-04 08:52:13 +0530

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,11 +1,9 @@
 n=int(input())
 f=0
-for i in range(1,n+1):
+for i in range(2,n):
     if (n%i==0):
-        f+=1
-    else:
-        continue
-if(f==2):
-    print(n,"is Prime")
+      print(n,"is not Prime")
+      break
 else:
-    print(n,"is not Prime")
+  print(n,"is Prime")
+    
